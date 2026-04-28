88bb5043e560317f33d231ae1b52e8923ae2002e
Aaravcodespython <sheeta08@googlemail.com>
2026-04-28 16:59:02 +0100

    Update main.py

--- Quiz Game/main.py
+++ Quiz Game/main.py
@@ -7,8 +7,17 @@ import pgzrun
 WIDTH = 870
 HEIGHT = 650
 
+score = 0
+time_left = 10
+game_over = False
+marquee_message = "Welcome to Quiz Game!"
+question_count = 0
+question_index = 0
+file_name = "questions.txt"
+questions = []
+
 #creating the rectangles
-marquee_box = Rect(0,0,800,80)
+marquee_box = Rect(0,0,870,80)
 question_box = Rect(0,0,650,150)
 timer_box = Rect(0,0,150,150)
 answer_box1 = Rect(0,0,300,150)
@@ -25,17 +34,25 @@ answer_box3.move_ip(20,450)
 answer_box4.move_ip(370,450)
 skip_box.move_ip(700,270)
 
+answer_boxes = [answer_box1, answer_box2, answer_box3, answer_box4]
+
 def draw():
+    global marquee_message
     screen.fill("white")
 
     #drawing the rectangles on the screen
-    screen.draw.filled_rect(marquee_box,color="red")
-    screen.draw.filled_rect(question_box,color="yellow")
-    screen.draw.filled_rect(timer_box,color="purple")
-    screen.draw.filled_rect(answer_box1,color="green")
-    screen.draw.filled_rect(answer_box2,color="green")
-    screen.draw.filled_rect(answer_box3,color="green")
-    screen.draw.filled_rect(answer_box4,color="green")
-    screen.draw.filled_rect(skip_box,color="blue")
+    screen.draw.filled_rect(marquee_box, color="red")
+    screen.draw.filled_rect(question_box, color="yellow")
+    screen.draw.filled_rect(timer_box, color="purple")
+    screen.draw.filled_rect(answer_box1, color="green")
+    screen.draw.filled_rect(answer_box2, color="green")
+    screen.draw.filled_rect(answer_box3, color="green")
+    screen.draw.filled_rect(answer_box4, color="green")
+    screen.draw.filled_rect(skip_box, color="blue")
+
+    #adding texts on rectangles
+    marquee_message += f" Q: {question_index} out of {question_count}"
+    screen.draw.textbox(marquee_message, marquee_box, color="white")
+    screen.draw.textbox(str(time_left), timer_box, color="white", shadow=(0.5,0.5), scolor = "black")
 
 pgzrun.go()
